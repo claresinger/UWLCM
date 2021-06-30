4d568be60691c2728dafaf8a44d9f108124e19e8
claresinger <csinger@caltech.edu>
2021-06-30 14:10:41 -0700

    lengthen timescales further

--- src/opts/opts_lgrngn.hpp
+++ src/opts/opts_lgrngn.hpp
@@ -178,10 +178,10 @@ void setopts_micro(
     if(user_params.ccn_relax)
     {
       rt_params.cloudph_opts_init.rlx_switch = 1;
-      rt_params.cloudph_opts_init.rlx_bins = 100;
+      rt_params.cloudph_opts_init.rlx_bins = 10;
       rt_params.cloudph_opts_init.rlx_sd_per_bin = 40; // originally 100
-      rt_params.cloudph_opts_init.supstp_rlx = 600 / rt_params.dt; // 10 mins, originally relaxation every two minutes
-      rt_params.cloudph_opts_init.rlx_timescale = 1200; // 20 mins, originally 10 min
+      rt_params.cloudph_opts_init.supstp_rlx = 1800 / rt_params.dt; // 30 mins, originally relaxation every two minutes
+      rt_params.cloudph_opts_init.rlx_timescale = 3600; // 60 mins, originally 10 min
 
       // define kappa ranges of user-defined aerosol distros
       std::pair<thrust_real_t, thrust_real_t> user_kpa_rng1, user_kpa_rng2;
